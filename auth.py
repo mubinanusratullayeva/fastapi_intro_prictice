8f10c767c09224da76ac33576cf5e75b1652654f
smb <mubinanusratullayeva1@gmail.com>
2024-05-29 09:55:37 +0500

    added register post

--- auth.py
+++ auth.py
@@ -1,4 +1,11 @@
 from fastapi import APIRouter
+from fastapi import HTTPExpection
+from fastapi import status
+
+from db.database import Session, ENGINE
+from schemas import RegisterModel
+from db.models import User
+
 
 auth_router = APIRouter(prefix='/auth')
 
@@ -7,6 +14,23 @@ async def register():
     return {
         "message": "Sign Up"
     }
+
+
+@auth_router.get('/register')
+async def register_post(user: RegisterModel):
+    email = Session.query(User).filter(User.email == user.email).first()
+    username = Session.query(User).filter(User.username == user.username).first()
+    if username or email is not None:
+        return HTTPExpection(status_code=status.HTTP_400_BAD_REQUEST, detail="this user is already exist")
+
+
+    new_user = User(
+        username=db_username,
+        email=user.email,
+        password=user.password,
+        is_staff=user.is_staff,
+        is_active=user.is_active
+    )
 
 
 @auth_router.get('/login')
---
+++ schemas.py
@@ -0,0 +1,16 @@
+from pydantic import BaseModel
+
+
+class RegisterModel(BaseModel):
+    id: Optional[int]
+    username: str
+    email: str
+    password: str
+    is_staff: Optional[bool]
+    is_active: Optional[bool]
+
+    class Config:
+        orm_mode = True
+        schema_extra = {
+            "example": {"Base example"}
+        }
