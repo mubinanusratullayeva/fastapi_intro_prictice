11c073e485f339426088baca753ced27c3c82ee0
smb <mubinanusratullayeva1@gmail.com>
2024-05-30 22:13:41 +0500

    added order and category routers

--- schemas.py
+++ schemas.py
@@ -9,8 +9,27 @@ class RegisterModel(BaseModel):
     is_staff: Optional[bool]
     is_active: Optional[bool]
 
-    class Config:
-        orm_mode = True
-        schema_extra = {
-            "example": {"Base example"}
-        }
+    # class Config:
+    #     orm_mode = True
+    #     schema_extra = {
+    #         "example": {"Base example"}
+    #     }
+
+
+class OrderModel(BaseModel):
+    id: Optional[int]
+    user_id: int
+    product_id: int
+
+
+class CategoryModel(BaseModel):
+    id: Optional[int]
+    name: str
+
+
+class ProductModel(BaseModel):
+    id: Optional[int]
+    name: str
+    description: str
+    price: float    
+    category_id: Optional[int]
